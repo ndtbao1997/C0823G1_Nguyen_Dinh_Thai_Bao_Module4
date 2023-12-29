0c03148bd87921bd71b84f2bfaf3f827fa83c98c
ndtbao1997 <nguyendinhthaibao63@gmail.com>
2023-12-29 14:20:41 +0700

    *backup

---
+++ bai_9/prac/customer_management/src/main/java/com/example/customer_management/logger/Logger.java
@@ -0,0 +1,27 @@
+package com.example.customer_management.logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class Logger {
+
+    @AfterThrowing(pointcut = "execution(public * com.example.customer_management.service.CustomerService.findAll(..))", throwing = "e")
+    public void logMethod(Exception e) {
+        System.out.println("[CMS] co loi xay ra: " + e.getMessage());
+    }
+
+    @AfterThrowing(pointcut = "execution(public * com.example.customer_management.service.CustomerService.*(..))", throwing = "e")
+    public void logClass(JoinPoint joinPoint, Exception e) {
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        System.out.printf("[CMS] co loi xay ra: %s.%s%s: %s%n", className, method, args, e.getMessage());
+    }
+}
